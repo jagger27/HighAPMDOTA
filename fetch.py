52c4629600bc7d83cfbd24d85b60e0482fd54f12
jagger27 <jagger27.com@gmail.com>
2014-06-16 02:04:53 -0400

    all heroes

--- fetch.py
+++ fetch.py
@@ -6,19 +6,30 @@ class RedisConnection(object):
 	def __init__(self):
 		self.r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-def parseMatchup(hero, r):
+def parseMatchup(hero, pipe):
 	heroHTML = Soup(fetchHero(hero))
-	pipe = r.pipeline()
 
 	for link in heroHTML.find_all("a", class_="hero-link"):
 		pipe.zadd(str(hero),
 				  float(link.parent.find_next_sibling().contents[0][:-1]),
 				  str(link.string))
 		# advantages[str(link.string)] = float(link.parent.find_next_sibling().contents[0][:-1])
-	pipe.execute()
 
 def fetchHero(hero):
-	return urllib2.urlopen("http://dotabuff.com/heroes/" + str(hero) + "/matchups").read()
+	return urllib2.urlopen("http://dotabuff.com/heroes/" + str(hero).lower().replace(' ','-').replace("'",'') + "/matchups").read()
 
 def fetchHeroList():
 	return urllib2.urlopen("http://dotabuff.com/heroes").read()
+
+def parse(r):
+	pipe = r.pipeline()
+
+	listHTML = Soup(fetchHeroList())
+
+	for link in listHTML.find_all("div", class_="name"):
+		hero = str(link.string)
+		parseMatchup(hero, pipe)
+
+	pipe.execute()
+
+parse(RedisConnection())
