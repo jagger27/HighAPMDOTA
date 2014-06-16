fddd7f01b835b4af9f3188ef82b9b2a2783df9f6
jagger27 <jagger27.com@gmail.com>
2014-06-16 01:23:38 -0400

    parseMatchup fix

--- fetch.py
+++ fetch.py
@@ -6,12 +6,15 @@ class RedisConnection(object):
 	def __init__(self):
 		self.r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-def parseMatchup(hero):
+def parseMatchup(hero, r):
 	heroHTML = Soup(fetchHero(hero))
+	pipe = r.pipeline()
 
 	for link in heroHTML.find_all("a", class_="hero-link"):
+		pipe.zadd(float(link.parent.find_next_sibling().contents[0][:-1]),
+				  str(link.string))
 		# advantages[str(link.string)] = float(link.parent.find_next_sibling().contents[0][:-1])
-
+	pipe.execute()
 
 def fetchHero(hero):
 	return urllib2.urlopen("http://dotabuff.com/heroes/" + str(hero) + "/matchups").read()
