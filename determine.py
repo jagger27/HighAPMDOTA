d345a98a8b4ca5d101cea3a6b707f80ad4984a80
jagger27 <jagger27.com@gmail.com>
2014-06-19 04:11:56 -0400

    rateCounter
    how one team's heroes counter the other's. NOT SYMMETRICAL

--- determine.py
+++ determine.py
@@ -8,3 +8,16 @@ def findCounter(r, hero, n=1):
 	pipe.zrevrange('vs '+hero, 0, n-1, 'WITHSCORES')
 
 	return pipe.execute()
+
+def rateCounter(r, team1, team2):
+	score = []
+
+	pipe = r.pipeline()
+
+	c = 0
+	for h in team1:
+		for g in team2:
+			score[c] += r.zscore(g, h)
+		c++
+
+	return score
