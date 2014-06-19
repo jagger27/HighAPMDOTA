03b05e9610465f546221a7c8f2f787aa2461c8e4
jagger27 <jagger27.com@gmail.com>
2014-06-19 04:13:41 -0400

    Revert "rateCounter"
    This reverts commit d480c937ed50c661d84adc3e10e23ad4244c3d4b.

--- determine.py
+++ determine.py
@@ -8,18 +8,3 @@ def findCounter(r, hero, n=1):
 	pipe.zrevrange('vs '+hero, 0, n-1, 'WITHSCORES')
 
 	return pipe.execute()
-
-	return pipe.execute()
-
-def rateCounter(r, team1, team2):
-	score = []
-
-	pipe = r.pipeline()
-
-	c = 0
-	for h in team1:
-		for g in team2:
-			score[c] += r.zscore(g, h)
-		c += 1
-
-	return score
